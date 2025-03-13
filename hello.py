9ace90152aebd61bc3f20984a9908d3e1d026b95
LorenDavie <loren.davie@gmail.com>
2025-03-13 11:27:15 -0400

    Initial positive test with bare script.

---
+++ hello.py
@@ -0,0 +1,24 @@
+"""
+Hello world.  I live inside supervisord, inside a docker container.
+"""
+
+import logging
+import sys
+
+log = logging.getLogger(__file__)
+
+
+def main():
+    logging.basicConfig(
+        level=logging.DEBUG,  # Set log level to DEBUG
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)],  # Write logs to stdout
+    )
+
+    print("Hello world, about to log.")
+    log.info("I am logging inside supervisord, inside docker.")
+    print("I have now logged.")
+
+
+if __name__ == "__main__":
+    main()
